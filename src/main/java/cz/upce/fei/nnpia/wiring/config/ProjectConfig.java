86438d731de48b114b0fe5efa63e1d2c4df5a506
miba1018 <michael.bazant@upce.cz>
2026-03-03 16:20:15 +0100

    uprava

--- src/main/java/cz/upce/fei/nnpia/wiring/config/ProjectConfig.java
+++ src/main/java/cz/upce/fei/nnpia/wiring/config/ProjectConfig.java
@@ -9,18 +9,21 @@ import org.springframework.context.annotation.Configuration;
 public class ProjectConfig {
 
     @Bean
-    Vehicle vehicle() {
-        Vehicle vehicle = new Vehicle();
-        vehicle.setName("Toyota");
-        return vehicle;
-    }
-
-    @Bean
     Person person(Vehicle vehicle) { // Using method parameters, preferovane vuci predchozimu pristupu
         Person person = new Person();
         person.setName("Lucy");
         person.setVehicle(vehicle); //manual wiring
         return person;
     }
+
+
+    @Bean
+    Vehicle vehicle() {
+        Vehicle vehicle = new Vehicle();
+        vehicle.setName("Toyota");
+        return vehicle;
+    }
+
+
 
 }
